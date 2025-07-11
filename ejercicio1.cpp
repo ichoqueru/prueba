91aca3d760d70014c0bdf25fe70d264ad0c2509f
ichoqueru <ichoquer@unjbg.edu.pe>
2025-07-10 23:02:27 -0500

    ejercicio de fraccion

---
+++ ejercicio1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,resultado;
+    resultado=0;
+
+    cout<<"Ingrese el valor de a: "; cin>>a;
+    cout<<"Ingrese el valor de b: "; cin>>b;
+
+    resultado=(a/b) +1;
+
+    cout<<"El resultado es: "<<resultado<<endl;
+
+
+
+    return 0;
+}
